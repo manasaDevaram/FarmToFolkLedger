9d8e541dc2c4cb7b5e28163ea60a1f786a612bf7
manasa <reddydmanasa@gmail.com>
2026-06-17 07:34:03 +0530

    Added base setup

---
+++ src/main/java/com/farmtofolk/farmtofolk_ledger/FarmToFolkLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.farmtofolk.farmtofolk_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmToFolkLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmToFolkLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmtofolk/farmtofolk_ledger/FarmToFolkLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmtofolk.farmtofolk_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmToFolkLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
