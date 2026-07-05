13b478cac44f96f4d6f51864d50a8493bcaa47da
manasa <reddydmanasa@gmail.com>
2026-07-05 21:04:19 +0530

    fix: use AWS default credentials for S3

--- src/main/java/com/farmtofolk/farmtofolk_ledger/storage/StorageProperties.java
+++ src/main/java/com/farmtofolk/farmtofolk_ledger/storage/StorageProperties.java
@@ -7,8 +7,6 @@ public class StorageProperties {
 
   private String region;
   private String bucket;
-  private String accessKey;
-  private String secretKey;
   private Integer presignedUrlExpiryMinutes;
 
   public String getRegion() {
@@ -25,22 +23,6 @@ public class StorageProperties {
 
   public void setBucket(String bucket) {
     this.bucket = bucket;
-  }
-
-  public String getAccessKey() {
-    return accessKey;
-  }
-
-  public void setAccessKey(String accessKey) {
-    this.accessKey = accessKey;
-  }
-
-  public String getSecretKey() {
-    return secretKey;
-  }
-
-  public void setSecretKey(String secretKey) {
-    this.secretKey = secretKey;
   }
 
   public Integer getPresignedUrlExpiryMinutes() {
